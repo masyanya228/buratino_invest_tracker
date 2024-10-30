b94446ec0dcd08526f926dc4b996484775ae0734
marsel.khabibullin.99 <marsel.khabibullin.99@gmail.com>
2024-10-30 10:32:22 +0300

    [feat]: T ToGrid KeyBoard Extention

--- Xtensions/DataXtentions.cs
+++ Xtensions/DataXtentions.cs
@@ -1,5 +1,7 @@
 ﻿using Buratino.Entities;
 
+using Telegram.Bot.Types.ReplyMarkups;
+
 namespace Buratino.Xtensions
 {
     public static class DataXtentions
@@ -15,6 +17,11 @@ namespace Buratino.Xtensions
                 result.Add(data.Where(x => x.Value.Count > i).Sum(x => x.Value[i]));
             }
             return result;
+        }
+
+        public static IEnumerable<IEnumerable<InlineKeyboardButton>> ToGrid<T>(this IEnumerable<T> filters, Func<T, string> titleSelector, Func<T, string> callbackSelector, int columnCount = 2)
+        {
+            return filters.Chunk(columnCount).Select(x => x.Select(y => new InlineKeyboardButton(titleSelector(y)) { CallbackData = callbackSelector(y) }));
         }
     }
 }
